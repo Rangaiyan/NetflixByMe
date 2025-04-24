160eff37346e1206846b6c68d01922c1ca1a73e4
Rangaiyan <rangaiyan29@gmail.com>
2025-04-24 13:00:53 +0530

    feat : added ui folder

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable({})
 export class AuthService {
-  login() {}
-  signup() {}
+  login(dto:AuthDto) {
+    // return 'user signed in'
+    return{
+        message:'user signed in',
+        data:dto
+    }
+  }
+  signup(dto:AuthDto) {
+    return{
+        message:'user signed upp!',
+        data:dto
+    }
+  }
 }
